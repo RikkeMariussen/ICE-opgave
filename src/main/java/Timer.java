c81786a2e6178273d73bd0da81f68c951036d432
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-05-08 11:51:42 +0200

    små ting til ball drop

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,2 +1,27 @@
-public class Timer {
+import processing.core.PApplet;
+
+public class Timer extends PApplet {
+
+    int startTime; //For the dropBalls
+    int interval;
+
+    //Constructor
+    Timer(int timeInterval) {
+        interval = timeInterval;
+    }
+
+    //Methods
+    void startTimer() {
+        startTime = millis();
+    }
+
+
+    boolean complete() {
+        int elapsedTime = millis() - startTime;
+        if (elapsedTime > interval) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
