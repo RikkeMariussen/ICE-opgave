501b3f1f3eeddc904d335ab1b03aefb584b1ade7
Crispy212 <Christofferleisted@gmail.com>
2024-05-08 13:31:35 +0200

    Added brick (not done)

---
+++ src/main/java/BrickBreaker.java
@@ -0,0 +1,12 @@
+public class BrickBreaker {
+
+
+    public void BrickBreaker() {
+        for (int i = 1; i <= 50; i++) { //midderlertidligt for loop for placeringen af bricks
+            if (i % 5 == 0) {
+            }
+        }
+    }
+
+
+}
