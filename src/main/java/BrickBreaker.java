b0c3cb471854c56c5270dcaa2a208da0e34e30df
Crispy212 <Christofferleisted@gmail.com>
2024-05-08 13:31:35 +0200

    Added brick (not done)

--- src/main/java/BrickBreaker.java
+++ src/main/java/BrickBreaker.java
@@ -1,46 +1,12 @@
-public class BrickBreaker extends AGames{
-    @Override
-    public String getTitle() {
-        return null;
+public class BrickBreaker {
+
+
+    public void BrickBreaker() {
+        for (int i = 1; i <= 50; i++) { //midderlertidligt for loop for placeringen af bricks
+            if (i % 5 == 0) {
+            }
+        }
     }
 
-    @Override
-    public String playGame() {
-        return null;
-    }
 
-    @Override
-    public String setHighScore() {
-        return null;
-    }
-
-    @Override
-    public String getHighScore() {
-        return null;
-    }
-
-    @Override
-    public String keyPressed() {
-        return null;
-    }
-
-    @Override
-    public String howToPlay() {
-        return null;
-    }
-
-    @Override
-    public String youDied() {
-        return null;
-    }
-
-    @Override
-    public String difficulties() {
-        return null;
-    }
-
-    @Override
-    public String lives() {
-        return null;
-    }
 }
