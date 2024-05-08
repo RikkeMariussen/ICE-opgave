f3cda4ab9663ebd50f277342ac7c065f3a52c39c
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-05-08 10:00:06 +0200

    Lavede interface til IGames og to af spillenes klasser som implementere IGames

---
+++ src/main/java/IGames.java
@@ -0,0 +1,11 @@
+public interface IGames {
+    String getTitle();
+    String playGame();
+    String setHighScore();
+    String getHighScore();
+    String keyPressed();
+    String howToPlay();
+    String youDied();
+    String difficulties();
+    String lives();
+}
---
+++ src/main/java/Snake.java
@@ -0,0 +1,46 @@
+public class Snake implements IGames {
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String playGame() {
+        return null;
+    }
+
+    @Override
+    public String setHighScore() {
+        return null;
+    }
+
+    @Override
+    public String getHighScore() {
+        return null;
+    }
+
+    @Override
+    public String keyPressed() {
+        return null;
+    }
+
+    @Override
+    public String howToPlay() {
+        return null;
+    }
+
+    @Override
+    public String youDied() {
+        return null;
+    }
+
+    @Override
+    public String difficulties() {
+        return null;
+    }
+
+    @Override
+    public String lives() {
+        return null;
+    }
+}
