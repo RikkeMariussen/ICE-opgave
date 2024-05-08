755381dd3bd8b1eb29e0459d6b5c04f9e677c428
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-05-08 10:25:00 +0200

    Klasser tilføjet: BrickBreaker GUI Input PacMan TextUI

---
+++ src/main/java/PacMan.java
@@ -0,0 +1,46 @@
+public class PacMan extends AGames{
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String playGame() {
+        return null;
+    }
+
+    @Override
+    public String setHighScore() {
+        return null;
+    }
+
+    @Override
+    public String getHighScore() {
+        return null;
+    }
+
+    @Override
+    public String keyPressed() {
+        return null;
+    }
+
+    @Override
+    public String howToPlay() {
+        return null;
+    }
+
+    @Override
+    public String youDied() {
+        return null;
+    }
+
+    @Override
+    public String difficulties() {
+        return null;
+    }
+
+    @Override
+    public String lives() {
+        return null;
+    }
+}
