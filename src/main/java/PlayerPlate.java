9b956785f5aa5cd7bc251d824ddde25679c436c5
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-05-09 09:27:19 +0200

    BallDrop arbejde

--- src/main/java/PlayerPlate.java
+++ src/main/java/PlayerPlate.java
@@ -1,2 +1,32 @@
-public class PlayerPlate {
+import processing.core.PApplet;
+
+public class PlayerPlate{
+    private PApplet parent;
+    private int size;
+    private int x;
+    private int y;
+    private int w;
+    private int h;
+    private int color;
+
+    public PlayerPlate(PApplet parent, int x, int y, int w, int h) {
+        this.parent = parent;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.w = 80;
+        this.h = 20;
+        this.color = parent.color(175);
+        //, int size, int x, int y, int w, int h, int color
+    }
+
+    public void update() {
+        x = parent.mouseX;
+    }
+
+    public void display() {
+        parent.fill(color);
+        parent.rect(x, y, w, h); //Rect for avoiding
+    }
+
 }
